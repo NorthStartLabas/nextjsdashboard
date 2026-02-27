aef8c891dc550d23671e344956bf923559ae06cd
Buterez <buterl2@medtronic.com>
2026-02-27 16:11:08 +0100

    fixed next config error

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: true,
-  },
   allowedDevOrigins: ["10.177.186.224", "localhost:3000"],
   experimental: {
     turbopackUseSystemTlsCerts: true,
